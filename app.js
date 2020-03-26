98b1b41376d84df057ae5e48bdffb90fee34f38e
Omoore94 <omoh.afekhume1@gmail.com>
2020-03-26 16:20:52 +0100

    looped through arrays for odd numbers

---
+++ app.js
@@ -0,0 +1,18 @@
+var name = "Paul Omoh";
+console.log(name);
+
+var courses = ["HTML", "Javascript", "CSS"];
+console.log(courses);
+
+len = courses.length;
+console.log(len);
+
+for(var i=0;i<=201;i++){
+
+if(len%2===1){
+    
+console.log(i + " is an odd number!\n")
+    
+}
+    
+ }
